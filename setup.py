8f3bcc900ea89c8f93a9d72d893c01494852d4b9
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2020-10-08 11:45:22 +0530

    new changes

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as help_file:
     long_description = help_file.read()
 
 setup(
-    name="hahano",
-    version=123.1,
-    description="hahano2",
-    long_description="lmfao"
+    name="hahano2",
+    version=123.21,
+    description="hahano3",
+    long_description="lmfao bruh"
 )
