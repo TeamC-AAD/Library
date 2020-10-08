43f8b9d37bb85998ec9861e3154dea6de9e14102
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2020-10-08 11:26:52 +0530

    random name change

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as help_file:
     long_description = help_file.read()
 
 setup(
-    name="GeneAlgorithmsLibrary",
-    version=0.0,
-    description="Gene library",
+    name="GTestAlot",
+    version=1.0,
+    description="Genes and genes",
     long_description=long_description,
     long_description_content_type="text/markdown"
 )
