5075f6b3859744db30453ad7fc4821f97c6524a5
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2020-10-08 11:41:21 +0530

    changed setup

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ with open("README.md", "r") as help_file:
     long_description = help_file.read()
 
 setup(
-    name="GTestAlot",
-    version=1.0,
-    description="Genes and genes",
-    long_description=long_description,
-    long_description_content_type="text/markdown"
+    name="Test",
+    version=10001,
+    description="Test",
+    long_description="lmfao"
 )
