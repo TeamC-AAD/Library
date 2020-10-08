87aabd6237d4b74e5276b5bc59a1110e15d49152
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2020-10-08 09:48:28 +0530

    Added some baseline setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as help_file:
+    long_description = help_file.read()
+
+setup(
+    name="GeneLibrary",
+    version=0.0,
+    description="Gene library",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
