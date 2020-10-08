9aabe04a809e07df73d7186cc513bcd9f0d0d49e
ArenaGrenade <56216597+ArenaGrenade@users.noreply.github.com>
2020-10-08 15:22:43 +0530

    PyPI integration (#1)
    Added some CI - might need to test if it works and correct it.

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as help_file:
+    long_description = help_file.read()
+
+setup(
+    name="hahano2",
+    version=123.21,
+    description="hahano3",
+    long_description="lmfao bruh"
+)
