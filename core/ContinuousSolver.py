da96f93dfaf2343c1bda5ac8cad7a8ca888ef292
abhishekh2001 <53903855+abhishekh2001@users.noreply.github.com>
2020-10-20 18:50:01 +0530

    Update ContinuousSolver.py

--- core/ContinuousSolver.py
+++ core/ContinuousSolver.py
@@ -149,4 +149,9 @@ class ContinuousGenAlgSolver(GenAlgSolver):
 
         return population
 
-    
+    #Temporary
+def get_input_dimensions(lst, n_dim=0):
+    if isinstance(lst, (list, tuple)):
+        return get_input_dimensions(lst[0], n_dim + 1) if len(lst) > 0 else 0
+    else:
+        return n_dim
