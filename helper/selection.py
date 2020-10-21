0c8ae829f86d88273efd1a43c4865ddc6af35608
Karthik Viswanathan <karthik.viswanathan@research.iiit.ac.in>
2020-10-21 17:07:49 +0400

    Crossover and Selection methods

--- helper/selection.py
+++ helper/selection.py
@@ -1,4 +1,6 @@
 """Defines all the available selection methods"""
+import sys
+import numpy as np
 
 def proportional_roulette_wheel():
     # TODO: Fill function
@@ -16,10 +18,22 @@ def linear_rank_with_selective_pressure():
     # TODO: Fill function
     pass
 
-def tournament_selection():
-    # TODO: Fill function
+def tournament_selection(pop,fitness,k):
+    '''
+    Input: population,fitness and a number k.
+    Output: the function conducts tournaments between k individuals randomly and selects the best
+    ''' 
+    N = np.size(pop)
+    best = -1
+    fit  = -sys.maxsize-1
+    for _ in range(1,k):
+        ind = pop[np.random.randint(0, N)]
+        if (fit == -1) or fitness[ind] > fitness[best]:
+            best = ind
+            fit = fitness[ind]
+    return best
+
+def boltzmann_selection(temp_gene , fitness , pop):
+    #TODO: Fill function
     pass
 
-def transform_ranking_selection():
-    # TODO: Fill function
-    pass
