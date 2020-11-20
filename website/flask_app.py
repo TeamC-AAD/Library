969fc34511355495999ebb56d0ab69bd588f4861
Rohan Asokan <rohan.asokan@students.iiit.ac.in>
2020-11-20 09:54:40 +0530

    started report page

--- website/flask_app.py
+++ website/flask_app.py
@@ -3,8 +3,12 @@ from flask import Flask, Response, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def mainpage():
     return render_template("index.html")
+
+@app.route('/report')
+def reportpage():
+    return render_template("report.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5050, debug=True)
